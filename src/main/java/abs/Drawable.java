0a44c316643bdda6b8f687b30d6188dee9fadd1e
Alex Malashevich <mwbox@mail.ru>
2023-07-10 19:32:54 +0300

    no message

--- src/main/java/abs/Drawable.java
+++ src/main/java/abs/Drawable.java
@@ -1,2 +1,48 @@
-package abs;public interface Drawable {
+package abs;
+
+public interface Drawable {
+
+    void takeAPencil();
+    void drawAShape();
+
+}
+
+class Circle implements Drawable{
+
+    @Override
+    public void takeAPencil() {
+
+    }
+
+    @Override
+    public void drawAShape() {
+
+    }
+}
+
+class Triangle implements Drawable, Printable{
+
+    @Override
+    public void takeAPencil() {
+        System.out.println("Take a pencil.");
+    }
+
+    @Override
+    public void drawAShape() {
+        System.out.println("Draw a triangle.");
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
+
+class Painter{
+    public static void main(String[] args) {
+        new Triangle().takeAPencil();
+        new Triangle().drawAShape();
+
+
+    }
 }
