db639eaa75dd436bb30a8dfdc156e0e9faef6f0d
Alex Malashevich <mwbox@mail.ru>
2023-08-21 21:48:14 +0300

    21.08.2023

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -1,7 +1,8 @@
 package util;
 
-import java.io.File;
+import java.io.*;
 import java.util.Date;
+import java.util.Locale;
 
 public class IOUtils {
 
@@ -28,4 +29,87 @@ public class IOUtils {
         }
     }
 
+    public static String readFile(String path){
+
+        String data = "";
+        try(Reader reader = new BufferedReader(
+                new FileReader(path))) { // using try - with -resources. See AutoCloseable
+            int characterCode;
+            while ((characterCode = reader.read()) != -1){
+                data += (char)characterCode;
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Check you file path");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static void write(String data, String path){
+        write(data, path, false);
+    }
+
+    public static void write(String data, String path, boolean append){
+        try ( Writer writer = new BufferedWriter(new FileWriter(path, append))){
+            writer.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Resource res = new Resource()){
+            res.addLine("Hello");
+            res.addLine(" World.");
+            res.printData();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyFiles(String dstPath, String path1, String path2){
+        String content1 = readFile(path1);
+        String content2 = readFile(path2);
+        write(content1 + content2, dstPath);
+    }
+
+    public static void copyFiles(String dstPath, String... paths){
+        StringBuilder sb = new StringBuilder();
+        for (String path : paths){
+            sb.append(readFile(path));
+        }
+        write(sb.toString(), dstPath);
+    }
+
+    public static void findText(String filePath, String searchText){
+        String content = readFile(filePath);
+        if(content.toLowerCase().contains(searchText.toLowerCase())){
+            System.out.println("Search text is found.");
+        }else{
+            System.out.println("Search text is not found.");
+        }
+    }
 }
+
+
+
+class Resource implements Closeable{
+    private String data = new String();
+
+    @Override
+    public void close() throws IOException {
+        if(data.length() > 0){
+            data = new String();
+            System.out.println("Resource data clear");
+        }
+    }
+    public void addLine(String line){
+        data += line;
+    }
+    public void printData(){
+        System.out.println(data);
+    }
+}
+
---
+++ src/test/java/util/IOUtilsTest.java
@@ -0,0 +1,64 @@
+package util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class IOUtilsTest {
+
+    @Test
+    public void readFileTest(){
+       String text = IOUtils.readFile("D:\\io_tests\\new_file.txt");
+       Assert.assertNotNull(text);
+       Assert.assertEquals("Wrong content", "Hello World.", text);
+    }
+
+    @Test
+    public void writeFileTest(){
+        String path = "D:\\io_tests\\new_file_" + new Date().getTime() + ".txt";
+        IOUtils.write("Hello from Java!", path);
+        Assert.assertTrue(IOUtils.readFile(path).length() > 10);
+    }
+
+    @Test
+    public void writeAppendFileTest(){
+        String path = "D:\\io_tests\\new_append_file_" + new Date().getTime() + ".txt";
+        IOUtils.write("Hello from Java!", path);
+
+        IOUtils.write(" EXTRA TEXT", path, true);
+        IOUtils.write(" TEXT2", path, true);
+        Assert.assertEquals("Wrong content",
+                "Hello from Java! EXTRA TEXT TEXT2", IOUtils.readFile(path));
+    }
+
+    @Test
+    public void copyFilesTest(){
+        String filePath1 = "D:\\io_tests\\first.txt";
+        String filePath2 = "D:\\io_tests\\second.txt";
+        String filePath3 = "D:\\io_tests\\third.txt";
+
+        IOUtils.copyFiles(filePath3, filePath1, filePath2);
+        Assert.assertEquals("Wrong content", "AB", IOUtils.readFile(filePath3));
+    }
+
+    @Test
+    public void copyMultFilesTest(){
+        String filePath1 = "D:\\io_tests\\A.txt";
+        String filePath2 = "D:\\io_tests\\B.txt";
+        String filePath3 = "D:\\io_tests\\C.txt";
+        String filePath4 = "D:\\io_tests\\D.txt";
+        String filePath5 = "D:\\io_tests\\E.txt";
+
+        IOUtils.write("A", filePath1);
+        IOUtils.write("B", filePath2);
+        IOUtils.write("C", filePath3);
+        IOUtils.write("D", filePath4);
+        IOUtils.write("E", filePath5);
+
+        String dstPath = "D:\\io_tests\\ABCDE.txt";
+
+        IOUtils.copyFiles(dstPath, filePath1, filePath2, filePath3, filePath4, filePath5);
+        Assert.assertEquals("Wrong content", "ABCDE", IOUtils.readFile(dstPath));
+    }
+}
